baac9bcc68945e0cbd89fd15d4a81ea7a59ce0e0
Hope_Davids <hopedavids@gmail.com>
2023-05-27 20:25:53 +0000

    admin python file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-"""  """
+""" define the url route to check the status for the app """
+
 from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """ Define the route """
     return jsonify({"status": "OK"})
 
