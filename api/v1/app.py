bb37b92aac9eed06430078700f2701e48cdd044d
Hope_Davids <hopedavids@gmail.com>
2023-05-27 20:30:12 +0000

    modified the python flask main app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,35 @@
-#!/usr/bin/python
-
-from flask import Flask
-from storage import models
+#!/usr/bin/python3
+"""
+Module app
+"""
 from api.v1.views import app_views
+from flasgger import Swagger
+from flask import (Blueprint, Flask, jsonify, make_response)
+from flask_cors import (CORS, cross_origin)
+from models import storage
+from os import getenv
 
-""" Creating and checking the status of the API """
 
 app = Flask(__name__)
-
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
+Swagger(app)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """json 404 page"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(exception):
+    """ closes the session """
     storage.close()
 
 
 if __name__ == "__main__":
-    import os
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenvv("HBNB_API_PORT", 5000)
-    app.run(host=host, port=port, thread=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+#    print(app.url_map)
+    app.run(host=host, port=port)
