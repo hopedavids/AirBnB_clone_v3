795eb646686c453bee1ff603e661e42694ebd878
Hope_Davids <hopedavids@gmail.com>
2023-05-27 20:17:25 +0000

    the init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python
+""" init py file that import the blueprint object"""
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""  """
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
+
